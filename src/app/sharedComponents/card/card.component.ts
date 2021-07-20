14341541d78a5762b20a55bc0b7de7af760fc57b
DrWho-cyber <77809141+DrWho-cyber@users.noreply.github.com>
2021-07-20 13:29:04 +0400

    getters and setters

--- src/app/sharedComponents/card/card.component.ts
+++ src/app/sharedComponents/card/card.component.ts
@@ -8,58 +8,67 @@ import { QuestionModel } from 'src/app/models/question.model';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  @Input()question:any;
-  questionObject:QuestionModel = new QuestionModel("","","","");
+   _question: any;
+  questionObject: QuestionModel = new QuestionModel("", "", "", "");
   answersArr: answersArrObjectModel[] = [];
+
+  @Input() set question(event:any){
+    this._question = event;
+    this.shuffle(this.answersArr);
+    this.CheckanswersArrcreat()
+  };
+
+  get question(){
+    return this._question
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
-    this.shuffle(this.answersArr);
-    this.CheckanswersArrcreat()
-    console.log("question " +  this.question, this.question.question)
   }
 
- CheckanswersArrcreat(){
-  this.questionObject.correct_answer = this.question.correct_answer;
-  this.questionObject.incorrect_answer1 = this.question.incorrect_answers[0];
-  this.questionObject.incorrect_answer2 = this.question.incorrect_answers[1];
-  this.questionObject.incorrect_answer3 = this.question.incorrect_answers[2];
-  return this.questionObject
- }
-  
-  
-  shuffle(array:any[]) {
+  CheckanswersArrcreat() {
+    this.questionObject.correct_answer = this.question.correct_answer;
+    this.questionObject.incorrect_answer1 = this.question.incorrect_answers[0];
+    this.questionObject.incorrect_answer2 = this.question.incorrect_answers[1];
+    this.questionObject.incorrect_answer3 = this.question.incorrect_answers[2];
+    return this.questionObject
+  }
+
+
+  shuffle(array: any[]) {
     let arrIndex = 0;
-    let answersArrObject:answersArrObjectModel = new answersArrObjectModel(`${this.question.correct_answer}`,true,"");
+    let answersArrObject: answersArrObjectModel = new answersArrObjectModel(`${this.question.correct_answer}`, true, "");
     array.push(answersArrObject);
-  this.question.incorrect_answers.forEach((element:any) => {
-    let answersArrObject2 = new answersArrObjectModel(`${element}`,false,"");
-    array.push(answersArrObject2);
-  });
-    var currentIndex = array.length,  randomIndex;
-  
+    this.question.incorrect_answers.forEach((element: any) => {
+      let answersArrObject2 = new answersArrObjectModel(`${element}`, false, "");
+      array.push(answersArrObject2);
+    });
+    var currentIndex = array.length, randomIndex;
+
     while (0 !== currentIndex) {
-  
+
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-  
+
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
     }
     arrIndex++
     console.log(array[arrIndex].correct)
-    
+
     return array;
   }
 
-  checkAnswer(answer:any){
-   if (answer.answer == this.questionObject.correct_answer) {
-    answer.classColorChange = "correctAnsw"
-  console.log(answer)
-   }else { answer.classColorChange = "incorrectAnsw"
-   console.log(answer);
-   
-  }
+  checkAnswer(answer: any) {
+    if (answer.answer == this.questionObject.correct_answer) {
+      answer.classColorChange = "correctAnsw"
+      console.log(answer)
+    } else {
+      answer.classColorChange = "incorrectAnsw"
+      console.log(answer);
+
+    }
 
   }
 
